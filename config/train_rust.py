48b8d566bf5f534dc95120d8b8060554e4969178
progerdakaasda <162299161+progerdakaasda@users.noreply.github.com>
2026-07-16 11:31:18 +0200

    Create train_rust.py

---
+++ config/train_rust.py
@@ -0,0 +1,42 @@
+out_dir = 'out-rust'
+
+dataset = 'rust'
+
+
+eval_interval = 500
+eval_iters = 100
+log_interval = 10
+
+always_save_checkpoint = True
+
+
+# model
+n_layer = 12
+n_head = 12
+n_embd = 704
+
+block_size = 1024
+
+dropout = 0.0
+bias = False
+
+
+# training
+batch_size = 8
+gradient_accumulation_steps = 8
+
+max_iters = 200000
+
+
+learning_rate = 3e-4
+min_lr = 3e-5
+
+warmup_iters = 2000
+
+weight_decay = 0.1
+
+beta1 = 0.9
+beta2 = 0.95
+
+
+compile = True
