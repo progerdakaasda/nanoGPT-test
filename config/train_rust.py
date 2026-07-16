62b00f947adc1803797f0dd4c105c84e55993ecc
progerdakaasda <162299161+progerdakaasda@users.noreply.github.com>
2026-07-16 12:37:37 +0200

    Update train_rust.py

--- config/train_rust.py
+++ config/train_rust.py
@@ -3,7 +3,7 @@ out_dir = 'out-rust'
 dataset = 'rust'
 
 
-eval_interval = 500
+eval_interval = 50
 eval_iters = 100
 log_interval = 10
 
@@ -15,7 +15,7 @@ n_layer = 12
 n_head = 12
 n_embd = 768
 
-block_size = 1024
+block_size = 512
 
 dropout = 0.0
 bias = False
@@ -39,4 +39,4 @@ beta1 = 0.9
 beta2 = 0.95
 
 
-compile = False
+compile = True
